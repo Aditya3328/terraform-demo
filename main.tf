905635078bd0e7af214e23c733ccf2ab9ca1985a
Aditya3328 <46055209+Aditya3328@users.noreply.github.com>
2023-04-26 09:18:22 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1" // set the AWS region you want to use
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-02eb7a4783e7e9317" // set the AMI ID of the image you want to use
+  instance_type = "t2.micro" // set the instance type you want to use
+  key_name      = "AssignmentKey" // set the name of the key pair you want to use to SSH into the instance
+  tags = {
+    Name = "demo-instance" // set a name for the instance
+  }
+}
